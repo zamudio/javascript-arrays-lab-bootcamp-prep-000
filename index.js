e3b943dc611baf50b42cbe9aa5fa6dcd6029d881
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-10 02:31:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  
+  var name = kittens.push(name)
+  return name
 }
